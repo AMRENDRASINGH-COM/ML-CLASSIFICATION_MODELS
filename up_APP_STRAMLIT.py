aaba039a7ce0a4e176e495b8a9e476e86772aa7e
AMRENDRASINGH-COM <amrendras439@gmail.com>
2024-09-26 14:47:13 +0530

    Update up_APP_STRAMLIT.py

--- up_APP_STRAMLIT.py
+++ up_APP_STRAMLIT.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
 import pickle
 
 with open ('final_model_gradient.pkl','rb') as file:
     model=pickle.load(file)
 
 
-def prediction (input_list):
+def prediction (lt,mst,spcl,price,adult,wkend,park,wk,ar_d,ar_m,ar_w):
     input_list=np.array(input_list,dtype='object')
 
     pred = model.predict_proba(input_list)[:,1][0]
@@ -35,7 +34,7 @@ def prediction (input_list):
         wwekd_lambda=(lambda x:0 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed' else 3 if x=='Thu' else 4 if x=='Fri' else 5 if x=='Sat' else 6) 
         arr_wd=wwekd_lambda(st.selectbox('WHAT IS THE WEEKDAY OF ARRIVAL?',['Mon','Tue','Wed','Thu','Fri','Sat']))
 
-        input_data=[[lt,mst,spcl,price,adult,wkend,park,wk,ar_d,ar_m,ar_w]]
+        input_list=[[lt,mst,spcl,price,adult,wkend,park,wk,ar_d,ar_m,ar_w]]
 
     if st.button('Predict'):
         responce=prediction(input_data)
